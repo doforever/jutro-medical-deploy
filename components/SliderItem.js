34ec0aeab2f5f34a867c8aaa6c47e895f45e156e
Dorota Tomala <doforever@gmail.com>
2022-03-09 23:14:31 +0100

    Set default mobile layout

--- components/SliderItem.js
+++ components/SliderItem.js
@@ -7,20 +7,20 @@ export default function SliderItem({ title, text, cta, cta_link, icon, img, clas
   const btnBg = className.includes('bg-blue') ? 'bg-green' : 'bg-blue';
 
   return (
-    <div className={`flex-col md:flex-row flex h-screen md:h-fit p-12 ${className}`}>
-      <div className='flex-1 flex flex-col justify-between'>
-        <Icon className='mb-14' name={icon} color={color}/>
-        <div className='flex-1 flex flex-col justify-between'>
-          <h1 className='font-header text-2xl sm:text-5xl'>{title}</h1>
-          <p className='sm:text-2xl'>{text}</p>
-          <div className={`px-4 py-2 ${btnBg} text-white self-start`}>
-            <Link  href={cta_link}>
-              <a>{cta}</a>
-            </Link>
-          </div>
+    <div 
+      className={`flex-col md:flex-row flex h-screen md:h-fit px-12 py-14 justify-between overflow-hidden ${className}`}
+    >
+      <div className='md:flex-1'>
+        <Icon className='mb-14' name={icon} color={color} />
+        <h1 className='font-header text-2xl sm:text-5xl mb-6'>{title}</h1>
+        <p className='sm:text-2xl mb-6'>{text}</p>
+        <div className={`px-4 py-2 ${btnBg} text-white inline-block`}>
+          <Link  href={cta_link}>
+            <a>{cta}</a>
+          </Link>
         </div>
       </div>
-      <div className='flex-1'>
+      <div className='md:flex-1 translate-y-16'>
         <Image src={img} alt={`${title} picture`} width={700} height={806} />
       </div>
     </div>
